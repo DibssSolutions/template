3482c43b5f0d9caffb2bf41ea9393ab2b3a52a6a
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-06-10 11:32:08 +0300

    revert

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,7 @@
 import svg4everybody from 'svg4everybody';
 import './common';
-import { BODY, NO_TOUCH, LOADED } from './constants';
+import { BODY, NO_TOUCH } from './constants';
 import { isTouch } from './utils';
 svg4everybody();
 
-
-BODY.addClass(LOADED);
 if (!isTouch()) BODY.addClass(NO_TOUCH);
